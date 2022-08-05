6d88b035a46eb748e4c1ad2a2d46790598a5d440
pengcheng-hou <hphilip168@gmail.com>
2022-08-04 22:40:44 -0400

    server res.write(),res.end()

--- 01.http-module/01.serverbase.js
+++ 01.http-module/01.serverbase.js
@@ -1,6 +1,15 @@
+const { contentType } = require('express/lib/response')
 var http = require('http')
 http.createServer((req,res)=>{
     console.log(req.url)
-    res.write('hello world')
+    // res.write('hello world')
+    // res.write('[1,2,3]')
+    // res.writeHead(200,{contentType:'text/html')
+    res.write(`
+    <html>
+    <h1> Hello China <h1/>
+    <html/>
+    
+    `)
     res.end()
 }).listen(3000,()=>{console.log("server is on at port: 3000")})
